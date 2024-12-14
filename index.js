1af3a0a55ff9538ed056116bc4946d2561626d73
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2024-12-14 22:21:35 +0700

    remove

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const defectRoutes = require('./src/routes/defectRoutes');
-const errorHandler = require('./src/middlewares/errorHandler');
 
 // Initialize express app
 const app = express();
@@ -19,9 +18,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/defects', defectRoutes);
-
-// Error handling middleware
-app.use(errorHandler);
 
 // Server setup
 const PORT = process.env.PORT || 5000;
