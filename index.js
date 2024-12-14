d51a9f1d39a0caccd9f491c12b2d7e6b41c08a5d
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2024-12-14 23:34:58 +0700

    Edit cors

--- index.js
+++ index.js
@@ -21,27 +21,8 @@ if (!fs.existsSync(uploadsPath)) {
     console.log('Uploads folder already exists at:', uploadsPath);
 }
 
-const allowedOrigins = [
-    "http://localhost:3000",
-    "https://home-savvy.vercel.app/"
-];
-
 // Middleware setup
-app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg =
-            'The CORS policy for this site does not allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
-      optionsSuccessStatus: 200,
-      credentials: true,
-    }),
-  );
+app.use(cors());
 app.use(bodyParser.json());  // JSON parser middleware (used instead of body-parser)
 
 // Static file serving for uploads
